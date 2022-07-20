d4ffc8b4b6cab33500e061fbcc3b0a15a6e5d32c
sanch <snk324@stern.nyu.edu>
2022-07-20 16:13:04 +0530

    this is a push in git

---
+++ mongotest.py
@@ -0,0 +1,19 @@
+import pymongo
+client = pymongo.MongoClient("mongodb+srv://test:TEST@atlascluster.npkvi.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+#creating a dictionary
+d = {
+	"name":"Sanchit",
+	"email":"snk324@stern.nyu.edu",
+	"surname":"Kumar"
+}
+
+#inserting the dictionary into mongodb
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
+
+
+print("hello")
